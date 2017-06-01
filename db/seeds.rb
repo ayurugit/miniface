346ca8a007c7b6f96a5193d32b387c5421f85a09
Ayuru <ayu@nttpc.co.jp>
2017-06-01 00:00:46 +0000

    dive09-2

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,23 +1,3 @@
-100.times do |n|
-  email = Faker::Internet.email
-  password = "password"
-  User.create!(email: email,
-               password: password,
-               password_confirmation: password,
-               )
-end
-
-   n = 1
-   while n <= 100
-   Blog.create(
-   title: "Blog",
-   content: "seed",
-   user_id: n
-   )
-  n = n + 1
-
-end
-
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
@@ -25,3 +5,28 @@ end
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+100.times do |n|
+  email = Faker::Internet.email
+  password = "password"
+  name = Faker::HarryPotter.character
+  User.create!(email: email,
+               password: password,
+               password_confirmation: password,
+               name: name
+               )
+end
+
+ n = 1
+ while n <= 100
+    title = Faker::HarryPotter.book
+    content = Faker::HarryPotter.quote
+  Blog.create(
+    title: title,
+    content: content,
+    user_id: n
+   )
+  n = n + 1
+   
+ end
+ 
