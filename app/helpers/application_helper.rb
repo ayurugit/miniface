08b846b611f97ad0701183f430dac08c4b2e17a8
Ayuru <ayu@nttpc.co.jp>
2017-07-06 11:08:52 +0000

    Dive14sns-0706

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,12 @@
 module ApplicationHelper
-　def profile_img(user)
+  def profile_img(user)
     return image_tag(user.avatar, alt: user.name) if user.avatar?
- 
+
     unless user.provider.blank?
       img_url = user.image_url
     else
       img_url = 'no_image.png'
     end
     image_tag(img_url, alt: user.name)
+  end
 end
