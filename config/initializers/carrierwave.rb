1d30abc181e6c777dbdf5726c23d7ef3dc8bf3af
Ayuru <ayu@nttpc.co.jp>
2017-10-01 02:02:24 +0000

    AWS-deploy CSS-edit

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -13,9 +13,9 @@ CarrierWave.configure do |config|
   case Rails.env
     when 'production'
       config.fog_directory = 'miniface-production'
-      config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/本番環境用のバケット名'
+      config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/miniface-production'
     when 'development'
       config.fog_directory = 'miniface-development'
-      config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/開発環境用のバケット名'
+      config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/miniface-development'
   end
 end
